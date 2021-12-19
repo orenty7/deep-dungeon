81ce822540956396865a0a968504364212b95cec
PUKICHH <91535360+PUKICHH@users.noreply.github.com>
2021-12-19 22:42:47 +0300

    Add files via upload
    pep8 level.py

---
+++ level.py
@@ -0,0 +1,25 @@
+import pygame
+
+from player import Player
+from engine import Engine
+from settings import FPS
+
+# setting the class level
+class Level:
+    def __init__(self, player_pos, tiles, settings):
+        self.engine = None
+        self.player = Player(player_pos)
+        self.tiles = tiles
+
+        settings = {
+            'gravity': 1200
+        }
+        self.settings = settings
+
+    def init_engine(self, window):
+        self.engine = Engine(self.tiles, self.player, self.settings, window)
+
+    def tick(self, screen, events):
+        screen.fill('white')
+        self.engine.tick(1 / FPS)
+        self.engine.draw(screen)
