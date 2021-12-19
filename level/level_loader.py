c5792915deff7816d2fd5037045c10054a7e094f
Shlyakhtun Prokhor <shlyakhtun.prohor@gmail.com>
2021-12-19 13:05:42 +0300

    Added ability to put tile in one line

--- level/level_loader.py
+++ level/level_loader.py
@@ -10,23 +10,26 @@ def parse_level(lines):
     width, height = map(int, lines[0].split())
     player_pos = map(int, lines[1].split())
 
+    tiles_str = map(str.split, lines[2::])
+
     x, y = (0, 0)
     tiles = []
-    for line in lines[2::]:
-        if line in spritesheet:
-            tiles.append(Tile(
-                (x * tile_size, y * tile_size, tile_size, tile_size),
-                spritesheet[line]
-            ))
-        elif line != 'empty':
-            print('Error, wrong tile')
+    for line in tiles_str:
+        for tile in line:
+            if tile in spritesheet:
+                tiles.append(Tile(
+                    (x * tile_size, y * tile_size, tile_size, tile_size),
+                    spritesheet[tile]
+                ))
+            elif tile != 'empty':
+                print('Error, wrong tile')
 
-        x += 1
-        if x == width:
-            y += 1
-            x = 0
-        if y == height:
-            break
+            x += 1
+            if x == width:
+                y += 1
+                x = 0
+            if y == height:
+                break
 
     return Level(player_pos, tiles, None)
 
--- tiles/tile.py
+++ tiles/tile.py
@@ -5,7 +5,7 @@ from tile_loader import sprites
 class Tile:
     def __init__(self, rect, cors):
         self.rect = pygame.Rect(rect)
-        self.image = sprites[cors[0]][cors[1]]
+        self.image = sprites[cors[1]][cors[0]]
 
     def rectangle(self):
         return self.rect
